743f5ad9bdebb1acbd43e111948f73a98099455c
Riley <riley@rileybathurst.com>
2020-06-13 16:19:01 -0700

    super ugly front page works

---
+++ src/components/footer.js
@@ -0,0 +1,71 @@
+import { Link } from "gatsby"
+import React from "react"
+
+import Logo from "./logo"
+import Download from "./download"
+
+const Footer = () => (
+    <footer>
+        {/* <h3 className="screen-reader">Snowledge</h3> */}
+    
+        <h3 id="footer-title"><em>Down-<br />Load</em></h3>
+
+        <div className="footer-download-color-blocking">
+            {/* gray trapezoid */}
+            <svg title="download-color-blocking" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                <polygon fill="#F0F0F0" points="0,25 100,0 100,75 0,100"/>
+            </svg>
+        </div>
+
+        <div className="footer-download-badges">
+            <Download />
+        </div>
+
+        <div className="footer-download-badges-color-blocking">
+            <svg title="download-color-blocking" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                <polygon fill="#009D9B" points="0,25 100,0 100,75 0,100"/>
+            </svg>
+        </div>{/* footer-download-badges-color-blocking */}
+        
+        <div className="footer-color-blocking-primary">
+            <svg title="footer-color-blocking-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                <polygon fill="#009D9B" points="0,100 100,0 100,100"/>
+            </svg>
+        </div>
+
+        <div className="footer-color-blocking-black">
+            <svg title="footer-color-blocking-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                <polygon fill="#000" points="0,100 100,0 100,100"/>
+            </svg>
+        </div>
+
+        <div id="footer-color"> {/* needs a better name of this should be the top name */}
+            
+            <div className="footer-logo">
+                <Logo />
+            </div>
+
+            <p className="footer-copyright">© 2020 Snowledge Corp., All Rights Reserved.</p> {/* this punctuation is really wierd but its how it is on the current site */}
+            <nav>
+                <ul id="footer-info">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/download">Download App</Link></li>
+                    <li><Link to="/privacy-policy">Privacy Policy</Link></li>
+                    <li><Link to="/terms-of-use">Terms of Use</Link></li>
+                    <li><Link to="/affiliate-disclaimer">Affiliate Disclaimer</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/support">Support</Link></li>
+                </ul>
+            </nav>
+
+            <ul id="footer-social">
+                <li><a href to="https://www.instagram.com/snowledge.co/">instagram</a></li>
+                <li><a href to="https://www.facebook.com/snowledge.co">facebook</a></li>
+                <li><a href to="https://www.youtube.com/channel/UCtao0k3KRU8kur7pyPJPK0w">YouTube</a></li>
+                <li><a href to="https://twitter.com/snowledge_co">twitter</a></li>
+            </ul>
+        </div>{/* #footer-color */}
+    </footer>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,119 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+// import Layout from "../components/layout"
+import Footer from "../components/footer"
 import Image from "../components/image"
+import HeroImage from "../components/hero-image"
+import PhonePlaceholder from "../components/phonePlaceholder"
 import SEO from "../components/seo"
 
+import Logo from "../components/logo"
+import Download from "../components/download"
+
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+    <header id="header-hero">
+      <h1 className="screen-reader">Snowledge</h1> {/* always start with an h tag and make this screenreader friendly */}
+      <Logo />
+      <HeroImage />
+      <div className="header-hero_color-blocking">
+        <svg title="header-color-blocking" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <polygon fill="#239C9B" points="0,25 100,0 100,75 0,100"/>
+        </svg>
+      </div>
+
+      <button id="menu">Menu</button>
+      
+      <nav>
+        {/* <ul id="off-canvas">
+          <li><Link to="/#features">Features</Link></li>
+          <li><Link to="/team">Team</Link></li>
+          <li><Link to="/partner-resorts">Partner Resorts</Link>
+            <ul>
+              <li><Link to="/partner-resorts/west">West</Link></li>
+              <li><Link to="/partner-resorts/rockies">Rockies</Link></li>
+              <li><Link to="/partner-resorts/midwest">Midwest</Link></li>
+              <li><Link to="/partner-resorts/northeast">Northeast</Link></li>
+              <li><Link to="/partner-resorts/canada">Canada</Link></li>
+            </ul>
+          </li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li>
+            <ul>
+              <li><a href to="https://www.instagram.com/snowledge.co/">instagram</a></li>
+              <li><a href to="https://www.facebook.com/snowledge.co">facebook</a></li>
+              <li><a href to="https://www.youtube.com/channel/UCtao0k3KRU8kur7pyPJPK0w">YouTube</a></li>
+              <li><a href to="https://twitter.com/snowledge_co">twitter</a></li>
+            </ul>
+          </li>
+          <li><Link to="/download">Download</Link></li>
+        </ul> */}
+      </nav>
+
+      <div id="header-byline">
+        <h2><em>Snowledge Is Powder</em></h2>
+        <h3><em>Discover. Ride. Share.</em></h3>
+      </div>
+      
+      <Download />
+
+      <div className="bottom-block">
+        <svg title="header-color-blocking" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <polygon fill="#fff" points="0,100 100,0 100,100"/>
+        </svg>
+      </div>
+    </header>
+
+
+    <main id="home-main">
+      <h2 id="our-story-title"><em>Our<br />Story</em></h2>
+
+      {/* this will come from the api eventually */}
+      <p id="our-story">The Snowledge team loves to ski and ride but was frustrated with all of the different resort and GPS tracker apps needed to plan, track, and share that perfect day of skiing or snowboarding. Together, we knew we could do better. Designed with input from our community of industry gurus and resort experts, Snowledge combines all the features skiers and riders need, works at every resort and in the backcountry—and it’s free.</p>
+
+      <div className="main-color-blocking">
+        <svg title="header-color-blocking" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <polygon fill="#F0F0F0" points="0,25 100,0 100,75 0,100"/>
+        </svg>
+      </div>
+
+      {/* <CoverVideo /> */}
+      <div id="hero-video">
+        <iframe title="heroVideo" id="hero-video" width="560" height="315" src="https://www.youtube.com/embed/dq1UpXj0Iw0?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div className="video-color-blocking">
+          <svg title="header-color-blocking" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+            <polygon fill="#009D9B" points="0,25 100,0 100,75 0,100"/>
+          </svg>
+        </div>  
+      </div>
+
+      {/* <Gallery /> */}
+      {/* api is needed */}
+
+      <h2 id="features-title"><em>Why<br />Snowledge?</em></h2>
+      <p id="features-text">Snowledge is the only app you need on the mountain. Use GPS to get real-time info from resorts, track your day on the slopes, and easily find and locate your friends and family. Snowledge is Powder!</p>
+      <div className="features-color-blocking">
+        <svg title="features-color-blocking" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <polygon fill="#F0F0F0" points="0,25 100,0 100,75 0,100"/>
+        </svg>
+      </div>
+
+      <div className="home-phone-place">
+        <PhonePlaceholder />
+      </div>
+
+      <ul id="features-list">
+        <li>View current snow conditions, and research info on nearby resorts, offers, and current contests.</li>
+        <li>Stay connected with friends, family, and your favorite resorts, and see what skiers and riders around the world are up to.</li>
+        <li>Find your friends on the mountain and track your day with stats, recorded weather, and maps showing runs skied.</li>
+        <li>Customize your profile, bio, and gallery, and view detailed activity summaries of your days skied.</li>
+        <li>Get lift status updates, conditions, and parking information, as well as offers and contests from your favorite resorts.</li>
+      </ul>
+    </main>
+    <Footer />
+  </>
 )
 
 export default IndexPage
