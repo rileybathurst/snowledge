cc33085885c6d866de332a1dded8a41692b53229
Riley <riley@rileybathurst.com>
2020-07-08 16:53:29 -0700

    thanks page

---
+++ src/pages/thanks.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="Thanks" />
+    <h1>Newsletter got added</h1>
+    <p>🐱</p>
+  </Layout>
+)
+
+export default NotFoundPage
